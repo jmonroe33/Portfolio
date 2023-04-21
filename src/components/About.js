c6f02314b74f57490f0ad0b8cfa4c1009ae1c1ba
Jaden <jadenmonroe20001@outlook.com>
2023-04-20 17:47:46 -0700

    changed background gradients and about layout.

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,39 @@
 import React from 'react'
 import '../styles/about.scss'
 import { motion } from 'framer-motion'
-import AboutMe from '../assets/aboutMe.png'
+// import AboutMe from '../assets/aboutMe.png'
 
 function About() {
     return (
         <motion.div className='about'>
-             <img src={AboutMe} alt="About Me" className='about-me'/>
+             {/* <img src={AboutMe} alt="About Me" className='about-me'/> */}
 
              <div className='multi-container'>
-                <section className='text-container'>
-                    <h3>About</h3>
+                <section className='text-container-1'>
+                    <h3 className='about-h3'>About</h3>
                     <p>
-                        I'm a fullstack developer who has a deep passion for building software.  
+                        I'm a fullstack developer who has a deep passion for building software. 
+                        I started out as a American Sign Language major that was working at a popular food comapany known as Wendy's.   
+                    </p>
+                    <p>
+                    While working there I realized how inefficent thier POS is and wondered how I could build something like that.
+                        This led me down the path of learning the basics of programming and eventually choosing and graduating from a bootcamp.  
                     </p>
                 </section>
-                <section className='text-container'>
+
+                <section className='text-container-2'>
                     <h3>Skills</h3>
-                    <div className="skills">
+                  
                         <p>
-                            Soft Skills:blahhh blahh blahh
+                           Customer oriented, time management, task management, 
                         </p>
                         <p>
-                            Frontend Skills:HTML, CSS, Javascript, React, Bootstrap, Tailwind CSS 
+                            Frontend: HTML, CSS, Javascript, React, Bootstrap, Tailwind CSS 
                         </p>  
                         <p>
-                            Backend Skills: Node.js, Express.js, Knex, PostgreSQL     
+                            Backend: Node.js, Express.js, Knex, PostgreSQL     
                         </p>   
-                    </div>                 
+                       
                 </section>
              </div>
            
