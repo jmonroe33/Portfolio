c600bf4b488636a36cab1c427ea32cdafa497552
Jaden <jadenmonroe20001@outlook.com>
2023-05-01 14:31:39 -0700

    fixing nav in destop view

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,39 +5,41 @@ import { NavLink } from 'react-router-dom'
 
 
 function Navigation() {
-    
+
     return (
         <section className="nav" >
 
-            <div>
-                <NavLink to= "/" className='nav-btns'>
-                    {/* <FaHome className="icon-large"/> */}
-                    <p >Home</p>
-                </NavLink>
-            </div>
-            <div>
-                <NavLink to= "/projects" className='nav-btns' >
-                    {/* <FaLaptopCode className="icon-large" /> */}
-                    <p >Projects</p>
-                </NavLink> 
-            </div> 
-            <div>
-                <NavLink to= "/about" className='nav-btns'>
-                    {/* <FaUser className='icon-large'/> */}
-                    <p  >About</p>
-                </NavLink>  
-            </div>
-            <div className="">
-                <NavLink to="https://www.linkedin.com/in/jaden-monroe/">
-                    <FaLinkedin className='icon-large'/>
-                </NavLink> 
-            </div>
-            <div>
-                <NavLink to="https://github.com/jmonroe33">
-                    <FaGithub className='icon-large'/>
-                </NavLink>
-            </div>
-         
+            <section className='out-links'>
+                <div >
+                    <NavLink to="https://www.linkedin.com/in/jaden-monroe/">
+                        <FaLinkedin className='icon-large' />
+                    </NavLink>
+                </div>
+                <div>
+                    <NavLink to="https://github.com/jmonroe33">
+                        <FaGithub className='icon-large' />
+                    </NavLink>
+                </div>
+            </section>
+            <section className='in-links'>
+                <div>
+                    <NavLink to="/" className='nav-btns'>
+                        <p >Home</p>
+                    </NavLink>
+                </div>
+                <div>
+                    <NavLink to="/projects" className='nav-btns' >
+                        <p >Projects</p>
+                    </NavLink>
+                </div>
+                <div>
+                    <NavLink to="/about" className='nav-btns'>
+                        <p  >About</p>
+                    </NavLink>
+                </div>
+            </section>
+
+
         </section>
     )
 }
